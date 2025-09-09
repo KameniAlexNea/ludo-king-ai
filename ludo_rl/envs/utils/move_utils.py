6489e2c89cd6534d269bfc002bb6f50d6ddd72d4
eak <eak@ivalua.com>
2025-09-09 14:18:45 +0000

    refactor: Remove unused model_updated.py and streamline move_utils import

--- ludo_rl/envs/utils/move_utils.py
+++ ludo_rl/envs/utils/move_utils.py
@@ -1,4 +1,4 @@
 """Redirect to shared move utils in rl_base."""
 
 # Import the shared move utils
-from rl_base.envs.utils.move_utils import MoveUtils
+from rl_base.envs.utils.move_utils import MoveUtils # noqa: F401
